ac9e372e20e775b16fdf8cacf3afaec10ef0b493
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-03-18 20:32:21 +0100

    Táblázat megjelenítése/Lista bővítése plusz 1 fővel

--- jqscript.js
+++ jqscript.js
@@ -1,15 +1,16 @@
-const List = [{ nev: "Somogyi András ", tul1: "Szereti a halrudit ", tul2: "Magyar " }
-];
-
-$(function () {
-    const ARTICLE = $("article table");
-    for (let i = 0; i < List.length; i++) {
-        ARTICLE[i].innerHTML += "<tr>" + List[i].nev + "</tr";
-        ARTICLE[i].innerHTML += "<td>" + + List[i].tul1 +" "+ List[i].tul2 +"</td";
-
+let lista = [
+    { nev: "András", tul1: "halrudi", tul2: "Leage of Legends" },
+    { nev: "Cartmen", tul1: "kövér", tul2: "ideges" },
+    { nev: "Kyle", tul1: "Zsidó", tul2: "vörös hajú" },
+  ];
+  
+  $(function () {
+    const ARTICLEELEMHEAD = $("article table thead tr");
+    const ARTICLEELEMBODY = $("article table tbody tr");
+    for (let index = 0; index < lista.length; index++) {
+      ARTICLEELEMHEAD[0].innerHTML += "<th>"+lista[index].nev+"</th>";
+      ARTICLEELEMBODY[0].innerHTML += "<td>" + lista[index].tul1 + "</td>";
+      ARTICLEELEMBODY[0].innerHTML += "<td>" + lista[index].tul2 + "</td>";
     }
-
-    
-
-
-});
+  });
+  
