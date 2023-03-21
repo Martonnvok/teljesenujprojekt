e4429f3b0cc96a9437518544ce384f6b75be311d
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-03-21 21:16:28 +0100

    Hibák kijavítása/ console error elhárítva
    A functionnél volt egy kis problémám, hiányzott ott a metódus neve és a paraméter

--- jqscript.js
+++ jqscript.js
@@ -1,16 +1,16 @@
 let lista = [
-    { nev: "András", tul1: "halrudi", tul2: "Leage of Legends" },
-    { nev: "Cartmen", tul1: "kövér", tul2: "ideges" },
-    { nev: "Kyle", tul1: "Zsidó", tul2: "vörös hajú" },
-  ];
-  
-  $(function () {
-    const ARTICLEELEMHEAD = $("article table thead tr");
-    const ARTICLEELEMBODY = $("article table tbody tr");
-    for (let index = 0; index < lista.length; index++) {
-      ARTICLEELEMHEAD[0].innerHTML += "<th>"+lista[index].nev+"</th>";
-      ARTICLEELEMBODY[0].innerHTML += "<td>" + lista[index].tul1 + "</td>";
-      ARTICLEELEMBODY[0].innerHTML += "<td>" + lista[index].tul2 + "</td>";
-    }
-  });
-  
+  { nev: "András", tul1: "halrudi", tul2: "Leage of Legends" },
+  { nev: "Cartmen", tul1: "kövér", tul2: "ideges" },
+  { nev: "Kyle", tul1: "Zsidó", tul2: "vörös hajú" },
+];
+
+function main(lista) {
+  const ARTICLEELEMHEAD = $("article table thead tr");
+  const ARTICLEELEMBODY = $("article table tbody tr");
+  for (let index = 0; index < lista.length; index++) {
+    ARTICLEELEMHEAD[0].innerHTML += "<th>" + lista[index].nev + "</th>";
+    ARTICLEELEMBODY[0].innerHTML += "<td>" + lista[index].tul1 + "</td>";
+    ARTICLEELEMBODY[0].innerHTML += "<td>" + lista[index].tul2 + "</td>";
+  }
+};
+
