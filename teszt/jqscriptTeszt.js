75482553ab4cb3934db946cd3100fbb6ee64ad34
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-03-21 21:10:28 +0100

    Táblázat metódus tesztelései

--- teszt/jqscriptTeszt.js
+++ teszt/jqscriptTeszt.js
@@ -1,5 +1,27 @@
-QUnit.module('táblázat metódus tesztelése', function() {
-    QUnit.test('beolvasas', function(assert) {
-      assert.equal([index].nev);
-    });
+QUnit.module('1. táblázat lista elso tulajdonsaga', function () {
+  QUnit.test('nev', function (assert) {
+    assert.equal(lista.tul1);
   });
+
+});
+
+QUnit.module('2. Táblázat különböző elemei megjelenítése egyszerre', function () {
+  QUnit.test('nev', function (assert) {
+    assert.equal(lista.nev,lista.tul1,lista.tul2);
+  });
+
+});
+
+QUnit.module('3. táblázat lista neve', function () {
+  QUnit.test('nev', function (assert) {
+    assert.equal(lista.nev);
+  });
+
+});
+
+QUnit.module('4.táblázat lista 2-ik tulajdonsaga', function () {
+  QUnit.test('nev', function (assert) {
+    assert.equal(lista.tul2);
+  });
+
+});
